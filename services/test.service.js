e581fdede578646d9774580841db3f8474fc3004
zhangy <zhangy@ivymobi.com>
2018-08-02 16:48:04 +0800

    啊

--- services/test.service.js
+++ services/test.service.js
@@ -1,4 +1,7 @@
 "use strict";
+
+
+const XLSX = require('node-xlsx');
 module.exports={
 
   name:"test",
@@ -20,6 +23,31 @@ module.exports={
         "name":"rua"
       };
     },
+    son: async function () {
+
+      // console.log(dir);
+      var workbook = XLSX.parse("test.xlsx");
+
+      var result = [];
+      workbook.forEach(wb => {
+        var headers = wb.data.shift();
+        wb.data = wb.data.map(wbd => {
+          var wbdi = {};
+          headers.forEach((h, i) => {
+            wbdi[h] = wbd[i] || '';
+          });
+          // console.log(wbdi);
+          return wbdi;
+        });
+        result.push({
+          name: wb.name,
+          data: wb.data
+        });
+      });
+      
+      console.log(JSON.stringify(result[0].data));
+      return JSON.stringify(result[0].data);
+    }
 
 
   },
