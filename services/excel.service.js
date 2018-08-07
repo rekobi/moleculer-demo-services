4aa94ea2f4e6e70293af54582be3ca628dc0240e
zhangy <zhangy@ivymobi.com>
2018-08-07 17:40:41 +0800

    excel 图片处理并同步qiniu

--- services/excel.service.js
+++ services/excel.service.js
@@ -2,9 +2,16 @@
 
 
 const XLSX = require('node-xlsx');
+const exec = require('child_process').exec;
+const path = require('path');
+
+const xlsxPicMix = require('../mixins/xlsxPicMixin.js');
+
 module.exports={
 
   name:"excel",
+
+  mixins: [xlsxPicMix],
 
   version: 3,
 
@@ -13,93 +20,112 @@ module.exports={
     filepath: "string"
   },
 
+  
+
   actions:{
 
     json: async function (ctx) {
-      let filepath = ctx.params.filepath;
+      try {
+        let filepath = ctx.params.filepath;
+        let xmlPath = '';
+        await this.unzip(filepath,"filehenf").then((res)=>{
+          xmlPath = res;
 
-      let workbook = XLSX.parse(filepath);
-
-      let mapsData = [];
-      workbook.forEach(rows => {
-        let headers = rows.data.shift();
-        let temp = {};
-        rows.data = rows.data.map(row => {
-          let block = {};
-          for (var i = 0; i < row.length; i++){
-            if(row[i] !== undefined || row[1] !==undefined){
-              temp[i] = row[i] || "";
-            }
-            if(row[i] === undefined){
-              block[headers[i]] = temp[i];
-            }else{
-              block[headers[i]] = row[i];
-            }
-          }
-          return block;
         });
-        mapsData.push({
-          name: rows.name,
-          data: rows.data
+        let images = await this.getPicInfo(path.dirname(__dirname) + "/" + xmlPath);
+        await images.map((res)=>{
+          console.log(res);
+          ctx.call("v1.qiniu.upload", { fileName: "456.png", filePath: res.imagePath });
         });
-      });
-
-
-      let item = {},
-        endData ={
-          data : null,
-          other: null
-        },
-        returnData = [],
-        others = [];
-      for (let i = 0; i < mapsData[0].data.length; i++) {
-        let ai = mapsData[0].data[i];
-        if(!ai.对内配置){
-          let other = Object.values(ai);
-          if(JSON.stringify(other) != "[]"){
-            other.map((v)=>{
-              if(v.length !=0){
-                others.push(v);
-              }
-            });
-            
-          }
-        }else{
-          if (!item[ai.材料名称]) {
-            let row = Object.assign({},ai);
-            delete row.材料名称;
-            delete row.是否为必选;
-            delete row.是否为单选;
-            delete row.说明;
-            returnData.push({
-              name: ai.材料名称,
-              isMust:this.isTrue(ai.是否为必选),
-              isSingle: this.isTrue(ai.是否为单选),
-              description: ai.说明,
-              headers: ["对内配置","对外配置","成本/元"],
-              rows: [Object.values(row)],
-            });
-                
-            item[ai.材料名称] = ai;
-          } else {
-            for (let j = 0; j < returnData.length; j++) {
-              let temp = returnData[j];
-              if (temp.name == ai.材料名称) {
-                delete ai.材料名称;
-                delete ai.是否为必选;
-                delete ai.是否为单选;
-                delete ai.说明;
-                temp.rows.push(Object.values(ai));
-                break;
-              }
-            }
-          }
-        }
         
+        // console.log(d);
+
+        let workbook = XLSX.parse(filepath);
+
+        let mapsData = [];
+        workbook.forEach(rows => {
+          let headers = rows.data.shift();
+          let temp = {};
+          rows.data = rows.data.map(row => {
+            let block = {};
+            for (var i = 0; i < row.length; i++){
+              if(row[i] !== undefined || row[1] !==undefined){
+                temp[i] = row[i] || "";
+              }
+              if(row[i] === undefined){
+                block[headers[i]] = temp[i];
+              }else{
+                block[headers[i]] = row[i];
+              }
+            }
+            return block;
+          });
+          mapsData.push({
+            name: rows.name,
+            data: rows.data
+          });
+        });
+
+      
+        let item = {},
+          endData ={
+            data : null,
+            other: null
+          },
+          returnData = [],
+          others = [];
+        for (let i = 0; i < mapsData[0].data.length; i++) {
+          let ai = mapsData[0].data[i];
+          if(!ai.对内配置){
+            let other = Object.values(ai);
+            if(JSON.stringify(other) != "[]"){
+              other.map((v)=>{
+                if(v.length !=0){
+                  others.push(v);
+                }
+              });
+            
+            }
+          }else{
+            if (!item[ai.材料名称]) {
+              let row = Object.assign({},ai);
+              delete row.材料名称;
+              delete row.是否为必选;
+              delete row.是否为单选;
+              delete row.说明;
+              returnData.push({
+                name: ai.材料名称,
+                isMust:this.isTrue(ai.是否为必选),
+                isSingle: this.isTrue(ai.是否为单选),
+                description: ai.说明,
+                headers: ["对内配置","对外配置","成本/元"],
+                rows: [Object.values(row)],
+              });
+                
+              item[ai.材料名称] = ai;
+            } else {
+              for (let j = 0; j < returnData.length; j++) {
+                let temp = returnData[j];
+                if (temp.name == ai.材料名称) {
+                  delete ai.材料名称;
+                  delete ai.是否为必选;
+                  delete ai.是否为单选;
+                  delete ai.说明;
+                  temp.rows.push(Object.values(ai));
+                  break;
+                }
+              }
+            }
+          }
+        
+        }
+        endData.data = returnData;
+        endData.other = others;
+        return JSON.stringify(endData);
+      } catch (error) {
+        console.log(error);
       }
-      endData.data = returnData;
-      endData.other = others;
-      return JSON.stringify(endData);
+      
     }
 
 
@@ -121,6 +147,22 @@ module.exports={
       }else{
         return false;
       }
+    },
+    unzip: async function(filePath, unzipFolder) {
+      return await new Promise((resolve, reject) => {
+        let command = `unzip -o ${filePath} -d ${unzipFolder}`;
+        exec(command, (err) => {
+          if(err) { reject(err); }
+    
+          // let filePaths = stdout
+          // .split('\n')
+          // .map(i => i.split(': ')[1])
+          // .filter(i => i)
+          // .map(i => i.trim());
+          
+          resolve(unzipFolder + "/xl/drawings");
+        });
+      });
     }
   }
 
--- services/qiniuUpload.service.js
+++ services/qiniuUpload.service.js
@@ -10,12 +10,17 @@ module.exports = {
   settings: {
 
   },
+
+  params: {
+    fileName : "string",
+    filePath : "string"
+  },
     
   actions:{
 		
     upload: {
-      handler() {
-        return this.uploadFileToQiNiu();
+      handler(ctx) {
+        return this.uploadFileToQiNiu(ctx.params.fileName,ctx.params.filePath);
       }
     }
 
@@ -40,7 +45,7 @@ module.exports = {
 	 */
   methods: {
 		
-    uploadFileToQiNiu() {
+    uploadFileToQiNiu(fileName,filePath) {
       let bucket = property.qiniu.bucket;
       let accessKey = property.qiniu.ACCESS_KEY;
       let secretKey = property.qiniu.SECRET_KEY;
@@ -56,8 +61,10 @@ module.exports = {
       config.zone = qiniu.zone.Zone_z1;
       config.useCdnDomain = true;
       let resumeUploader = new qiniu.resume_up.ResumeUploader(config);
-      let key="test10M0.mp4";
-      let localFile = "/home/zouwb/projects/orbit/microServices/qiniu/uploadFileTest/test10M.mp4";
+      // let key="test10M0.mp4";
+      let key = fileName;
+      // let localFile = "/home/zouwb/projects/orbit/microServices/qiniu/uploadFileTest/test10M.mp4";
+      let localFile = filePath;
       let putExtra = new qiniu.resume_up.PutExtra();
       putExtra.resumeRecordFile = key + ".log";
       putExtra.progressCallback = function(uploadBytes, totalBytes) {
